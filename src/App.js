c366abe1742e966f2571f200b1436a744c0f60b4
이준화 <62980975+wnsghk16@users.noreply.github.com>
2020-08-05 12:02:52 +0900

    200805 12:02

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BookmarkMap from "./map/bookmark-map";
+import Map from "./map/map";
 
 function App() {
   return (
     <div className="App">
-      <BookmarkMap/>
+      <Map/>
     </div>
   );
 }
